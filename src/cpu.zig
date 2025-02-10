09665684bf332cf2cea88091b9ecbb582bfbb1c0
JD <ott.d.john@gmail.com>
2025-02-10 03:49:55 -0500

    additional reg

--- src/cpu.zig
+++ src/cpu.zig
@@ -12,6 +12,15 @@ pub const Registers = struct {
     sp: u16,
     pc: u16,
 
+    pub fn getAF(self: *Registers) u16 {
+        return @as(u16, self.a) << 8 | @as(u16, self.f);
+    }
+
+    pub fn setAF(self: *Registers, value: u16) void {
+        self.a = @intCast((value >> 8) & 0xFF);
+        self.f = @intCast(value & 0xFF);
+    }
+
     pub fn getBC(self: *Registers) u16 {
         return @as(u16, self.b) << 8 | @as(u16, self.c);
     }
@@ -19,6 +28,24 @@ pub const Registers = struct {
     pub fn setBC(self: *Registers, value: u16) void {
         self.b = @intCast((value >> 8) & 0xFF);
         self.c = @intCast(value & 0xFF);
+    }
+
+    pub fn getDE(self: *Registers) u16 {
+        return @as(u16, self.d) << 8 | @as(u16, self.e);
+    }
+
+    pub fn setDE(self: *Registers, value: u16) void {
+        self.d = @intCast((value >> 8) & 0xFF);
+        self.e = @intCast(value & 0xFF);
+    }
+
+    pub fn getHL(self: *Registers) u16 {
+        return @as(u16, self.h) << 8 | @as(u16, self.l);
+    }
+
+    pub fn setHL(self: *Registers, value: u16) void {
+        self.h = @intCast((value >> 8) & 0xFF);
+        self.l = @intCast(value & 0xFF);
     }
 
 };
