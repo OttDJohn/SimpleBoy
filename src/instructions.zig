8f8dce558f74b5d853a07f1f0a9821a45aa0bb60
JD <ott.d.john@gmail.com>
2025-02-17 13:10:32 -0500

    slight fix?

--- src/instructions.zig
+++ src/instructions.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
-const opcode = @import("opcodes.zig");
+const opcodes = @import("opcodes.zig");
 const c = @import("cpu.zig");
 
 
 pub fn execute(cpu: *c.SM83, op: u16) void {
     std.debug.print("0x{x}\n", .{op});
     return switch (op) {
-        opcode.NOP => {},
-        opcode.LD_BC_n16 => {
+        opcodes.opcode.NOP => {},
+        opcodes.opcode.LD_BC_n16 => {
             cpu.SM83.setBC(op);
         }
     };
